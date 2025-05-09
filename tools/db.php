1892c921753ee10bcf2676d63c0b9e7f9f950af9
afaith-a <arianefaithabsalon@gmail.com>
2025-05-09 12:41:39 +0800

    Update db.php

--- tools/db.php
+++ tools/db.php
@@ -1,1 +1,11 @@
 
+$host = 'sql312.infinityfree.com';
+$username = 'if0_38929999';  
+$password = 'faithfaith19';      
+$dbname = 'if0_38929999_faiths';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
