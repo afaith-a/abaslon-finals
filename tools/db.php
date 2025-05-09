f5b1f4a6bba5fcb04e964c3e11e7d83efb5610c4
afaith-a <arianefaithabsalon@gmail.com>
2025-05-09 12:43:07 +0800

    Update db.php

--- tools/db.php
+++ tools/db.php
@@ -1,3 +1,5 @@
+<?php
+function getDBConnection(){
 
 $host = 'sql312.infinityfree.com';
 $username = 'if0_38929999';  
@@ -9,3 +11,8 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+return $connection;
+}
+
+?>
